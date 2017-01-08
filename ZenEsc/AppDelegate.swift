dc55d56aa35513a03668270dd69497d6edaef888
s.murakami <smurakami.84@gmail.com>
2017-01-08 18:44:19 +0900

    Initial Commit

---
+++ ZenEsc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZenEsc
+//
+//  Created by 村上晋太郎 on 2017/01/08.
+//  Copyright © 2017年 村上晋太郎. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
